6af30ca3289bd76810de83c1ae1662f7174c9c78
Reena-porsiya <138755077+Reena-porsiya@users.noreply.github.com>
2024-01-17 16:19:36 +0530

    changes

--- frontend/angularprojects/angular-flights/src/app/app.module.ts
+++ frontend/angularprojects/angular-flights/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateCountryComponent } from './create-country/create-country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCountryComponent
   ],
   imports: [
     BrowserModule
